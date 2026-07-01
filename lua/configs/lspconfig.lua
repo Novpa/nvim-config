06a469c977ee9e3e501ec3fd975f1457f64065c8
Novpa <aa.novparadiputra@binus.ac.id>
2026-07-01 11:39:27 +0800

    feat: initial nvim config

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -1,6 +1,20 @@
-require("nvchad.configs.lspconfig").defaults()
 
-local servers = { "html", "cssls" }
-vim.lsp.enable(servers)
+vim.lsp.enable({ "html", "cssls" })
+
+-- Pylsp dengan config
+vim.lsp.config("pylsp", {
+  settings = {
+    pylsp = {
+      plugins = {
+        pycodestyle = {
+          ignore = { "W391" },
+          maxLineLength = 100
+        }
+      }
+    }
+  }
+})
+
+vim.lsp.enable("pylsp")
 
 -- read :h vim.lsp.config for changing options of lsp servers 
