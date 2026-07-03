0c54ac405ce398539885fe768d5c93238601e756
Novpa <aa.novparadiputra@binus.ac.id>
2026-07-03 09:02:55 +0800

    chore: edit options.lua

--- lua/options.lua
+++ lua/options.lua
@@ -1,5 +1,14 @@
 require "nvchad.options"
 
+local o = vim.o
+o.cursorlineopt = "both" -- to enable cursorline!
+o.background = "dark"
+vim.opt.relativenumber = true
+vim.opt.list = true
+vim.opt.listchars = {
+  eol = '↴',
+}
+
 -- add yours here!
 
 -- local o = vim.o
